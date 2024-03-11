f506918fcdc2d070b34dd06d3f7e398f9f734141
roxanap33 <roxana.ciocian@principal33.com>
2024-03-11 12:49:54 +0200

    Add theme to the application

--- google-clone-app/src/main.tsx
+++ google-clone-app/src/main.tsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    body: {
+      darkColor: string;
+    };
+  }
+
+  interface ThemeOptions {
+    body?: {
+      darkColor?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  body: {
+    darkColor: "rgba(32, 33, 36, 1)",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
