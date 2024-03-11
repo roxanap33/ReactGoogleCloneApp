f32a0e624d9d367b1856fc29058425eeeb8804f7
roxanap33 <roxana.ciocian@principal33.com>
2024-03-11 23:12:36 +0200

    Add text section with links after search buttons

--- google-clone-app/src/components/SearchInputStyles.tsx
+++ google-clone-app/src/components/SearchInputStyles.tsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/system";
 import { Search, Mic } from "@mui/icons-material";
 
 export const SearchContainer = styled("div")`
-  padding: 20px;
+  padding: 20px 20px 0 20px;
   display: flex;
   justify-content: center;
 `;
@@ -17,21 +17,6 @@ export const SearchInputContainer = styled("div")`
   border-radius: 300px;
   min-height: 30px;
   width: 60%;
-
-  //   textarea {
-  //     flex: 1;
-  //     background-color: rgba(32, 33, 36, 1);
-  //     border: none;
-  //     resize: none;
-  //     margin: 0 0 8px;
-  //     height: 20px;
-  //     word-wrap: break-word;
-  //     color: white;
-
-  //     &:focus {
-  //       outline-width: 0;
-  //     }
-  //   }
 `;
 
 export const SearchTextArea = styled("textarea")`
@@ -59,4 +44,45 @@ export const MicrofonIcon = styled(Mic)`
   font-size: 20px;
   margin-left: 5px;
   margin-bottom: 10px;
+`;
+
+export const SearchButtonsContainer = styled("div")`
+  display: flex;
+  justify-content: center;
+  padding: 18px 20px 20px 20px;
+`;
+
+export const SearchButtons = styled("button")`
+  background-color: #303134;
+  border: 1px solid #303134;
+  border-radius: 4px;
+  color: #e8eaed;
+  font-family: arial, sans-serif;
+  font-size: 14px;
+
+  margin: 11px 4px;
+  padding: 0 16px;
+  line-height: 27px;
+  height: 36px;
+  min-width: 54px;
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const TextSectionContainer = styled("div")`
+  font-size: small;
+  text-align: center;
+  margin-bottom: 24px;
+`;
+
+export const TextSection = styled("div")`
+  a {
+    padding: 0 3px;
+    color: ##8ab4f8;
+    text-decoration: none;
+
+    &:visited {
+      color: #c58af9;
+    }
+  }
 `;
