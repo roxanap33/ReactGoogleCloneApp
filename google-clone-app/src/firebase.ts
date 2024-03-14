b39720be388bfa6d07eaaf36f52524b1ff70761e
roxanap33 <roxana.ciocian@principal33.com>
2024-03-14 15:31:56 +0200

    Change firebase configuration keys

--- google-clone-app/src/firebase.ts
+++ google-clone-app/src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDY6gLM3ZfIy_E8dyGOni0GHQt0Dlay_FA",
-  authDomain: "react-clone-app-e94de-32603.firebaseapp.com",
-  projectId: "react-clone-app-e94de",
-  storageBucket: "react-clone-app-e94de.appspot.com",
-  messagingSenderId: "14017901318",
-  appId: "1:14017901318:web:5b051f0ad285e87cf1a487",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGE_SENDER_ID",
+  appId: "YOUR_API_ID",
 };
 
 const app = initializeApp(firebaseConfig);
